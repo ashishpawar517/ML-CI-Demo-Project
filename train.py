d58e1ab1647d5472aefa3f96c951785bc268d0d2
Aashish <pawarashish564@gmail.com>
2020-08-29 15:19:23 +0530

    💥 NEW: GradientBoostingClassifier

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
 
-
 # from xgboost import XGBClassifier
 # from lightgbm import LGBMClassifier
 # from sklearn.ensemble import RandomForestClassifier
@@ -20,18 +19,47 @@ y_test = np.genfromtxt("data/test_labels.csv")
 # from sklearn.naive_bayes import GaussianNB 
 # from sklearn.svm import SVC
 # from sklearn.linear_model import SGDClassifier
-from sklearn.neural_network import MLPClassifier
+# from sklearn.neural_network import MLPClassifier
 
-clf = MLPClassifier(random_state=1, max_iter=300)
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, BatchNormalization
+# from keras.wrappers.scikit_learn import KerasClassifier
 
+# def build_model():
+#     model = Sequential()
+#     model.add(Dense(64, activation='relu', input_dim=20, kernel_initializer='uniform'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(32, activation='relu', kernel_initializer='uniform'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(1, activation='sigmoid', kernel_initializer='uniform'))
+#     model.compile(optimizer = 'rmsprop', 
+#               loss='binary_crossentropy', metrics=['accuracy'])
+#     return model
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+clf =  GradientBoostingClassifier(n_estimators=20, learning_rate=0.1, max_features=2, max_depth=2, random_state=0)
 clf.fit(X_train,y_train)
 
+y_pred = clf.predict(X_test)
+
 acc = clf.score(X_test, y_test)
-print(acc)
+print('Accuracy : ',acc)
+
+from sklearn.metrics import classification_report
+report = classification_report(y_test, y_pred) 
+print(report)
+
 with open("metrics.txt", 'w') as outfile:
         outfile.write("Accuracy: " + str(acc) + "\n")
 
 
+f=open("metrics.txt", "a+")
+f.write("```")
+f.write(report)
+f.write("```")
+
+f.close()
 # Plot it
 disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
 plt.savefig('confusion_matrix.png')
